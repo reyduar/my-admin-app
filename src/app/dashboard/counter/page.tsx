bbc54fce0fc301364ff1015db3eae3a84e62ace2
“ariel.duarte” <arielpy@gmail.com>
2024-01-06 02:00:12 -0300

    Added shopping cart page

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const meta = {
+  title: "Shopping Cart",
+  description: "Cart items",
+};
+
 export default function CounterPage() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <span>Shopping Cart</span>
+        <CartCounter value={1} />
+      </div>
     </>
   );
 }
