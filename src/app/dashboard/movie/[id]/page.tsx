f5dc228d56545dcb9df1c0881b836900fd002230
“ariel.duarte” <arielpy@gmail.com>
2024-01-09 20:27:33 -0300

    Added mechanism to fetch movie by id from server component

--- src/app/dashboard/movie/[id]/page.tsx
+++ src/app/dashboard/movie/[id]/page.tsx
@@ -1,5 +1,86 @@
-import React from "react";
+import { MovieResponse } from "@/movies/interfaces";
+import { Metadata } from "next";
+import Image from "next/image";
 
-export default function MoviePage() {
-  return <div>MoviePage</div>;
+interface Props {
+  params: { id: string };
+}
+
+// Crear metadata dinamica para cada id de pelicula
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params;
+
+  const movie = await getMovie(id);
+
+  return {
+    title: movie.title,
+    description: movie.overview,
+  };
+}
+
+const getMovie = async (id: string): Promise<MovieResponse> => {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=de6715f3b6983b1e06ae712ebf052970&language=en-US&include_adult=true`,
+    {
+      cache: "force-cache",
+      next: {
+        revalidate: 60 * 60 * 24 * 30, // 30 days por el force-cache
+      },
+    }
+  ).then((res) => res.json());
+
+  console.log(`Se cargo a ${data.title}`);
+
+  return data;
+};
+
+export default async function MoviePage({ params }: Props) {
+  const { id } = params;
+
+  const movie = await getMovie(id);
+  return (
+    <div className="flex mt-5 flex-col items-center text-slate-800">
+      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
+        <div className="mt-2 mb-8 w-full">
+          <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
+            #{movie.id} {movie.title}
+          </h1>
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              width={400}
+              height={400}
+              alt={`Imagen del pokemon ${movie.title}`}
+              className="mb-5"
+            />
+
+            <div className="flex flex-wrap">
+              {movie.genres.map((gen) => (
+                <p key={gen.id} className="mr-2 capitalize">
+                  {gen.name}
+                </p>
+              ))}
+            </div>
+            <div className="flex flex-wrap">{movie.overview}</div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 px-2 w-full">
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
+            <p className="text-sm text-gray-600">Popularidad</p>
+            <span className="text-base font-medium text-navy-700 flex">
+              {movie.popularity}
+            </span>
+          </div>
+
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
+            <p className="text-sm text-gray-600">Votos</p>
+            <span className="text-base font-medium text-navy-700 flex">
+              {movie.vote_count}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/movies/interfaces/index.ts
+++ src/movies/interfaces/index.ts
@@ -1,2 +1,3 @@
 export type { MoviesReponse } from './movies-response';
 export type { SimpleMovie } from './simple-movie';
+export type { MovieResponse } from './movie-response';
